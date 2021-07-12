7b22fc7071b2c39181647306ab5a568a38dd71f5
Marcel <Marcel.Klotz@posteo.de>
2021-07-12 10:56:14 +0200

    Test Setup for the strapi backend, done like in a starter

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e633188b4987c795faf9c4743aaead3c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f8df6a61-f1f7-42ed-b4a0-9cf5b5864e86'
+};
